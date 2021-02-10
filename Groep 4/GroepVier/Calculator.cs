bcc28a7b4589bf9f19ef140b5217d70ed7ea2464
Dominick <d.meels@hotmail.com>
2021-02-10 10:22:47 +0100

    Added simple Calculator class
    Able to Add, Subtract, Divide and Multiply two integers

---
+++ Groep 4/GroepVier/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace GroepVier
+{
+    public class Calculator
+    {
+        public int Divide(int pNumber1, int pNumber2) => pNumber1 / pNumber2;
+
+        public int Add(int pNumber1, int pNumber2) => pNumber1 + pNumber2;
+
+        public int Subtract(int pNumber1, int pNumber2) => pNumber1 - pNumber2;
+
+        public int Multiply(int pNumber1, int pNumber2) => pNumber1 * pNumber2;
+    }
+}
